178d4637644005b1004763ae1470970f3e97533f
Alexander Gabriel <alex.barbalex@gmail.com>
2021-10-30 16:35:48 +0200

    up version

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -19,7 +19,7 @@ const Container = styled.div`
 const Layout = ({ children }) => (
   <Container>
     <Helmet
-      title="arteigenschaften v1.6.2"
+      title="arteigenschaften v1.6.3"
       meta={[
         {
           name: 'description',
