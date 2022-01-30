11589983cf55116e3f3f49a806feeab110f0f8b6
Alexander Gabriel <alex.barbalex@gmail.com>
2022-01-30 22:20:43 +0100

    work on tree query

--- src/sql/tree_by_active_url.sql
+++ src/sql/tree_by_active_url.sql
@@ -29,31 +29,27 @@
 WITH constants (
   active_url
 ) AS (
-  VALUES ('Arten/aed47d40-7b0e-11e8-b9a5-bd4f79edbcc4/aedf52b0-7b0e-11e8-b9a5-bd4f79edbcc4/aee40da0-7b0e-11e8-b9a5-bd4f79edbcc4')
+  VALUES ('Arten/aed47d40-7b0e-11e8-b9a5-bd4f79edbcc4/aedf52b0-7b0e-11e8-b9a5-bd4f79edbcc4/aee40da0-7b0e-11e8-b9a5-bd4f79edbcc4/aef3c510-7b0e-11e8-b9a5-bd4f79edbcc4/798695e7-65ea-4e63-90e4-c8a393233f2e')
 ),
 tree_categories AS (
   SELECT
     id, name, NULL::uuid AS parent_id, 1::bigint AS level, name AS category, replace(name, '/', '|') AS url,
-    replace(name, '/', '|') AS sort
+    sort AS cat_sort,
+    sort::text
   FROM
-    ae.tree_category,
-    constants c
-  WHERE
-    replace(name, '/', '|') = c.active_url
-    OR array_to_string(TRIM_ARRAY (regexp_split_to_array(c.active_url, '/'), 1), '/') = replace(name, '/', '|')
+    ae.tree_category
 ),
 taxonomies AS (
   SELECT
-    tax.id,
-    tax.name,
-    tax.tree_category AS parent_id,
-    2::bigint AS level,
-    cat.name AS category,
-    concat(replace(cat.name, '/', '|'), '/', tax.id) AS url,
+    tax.id, tax.name, tax.tree_category AS parent_id, 2::bigint AS level, cat.name AS category, concat(replace(cat.name, '/', '|'), '/', tax.id) AS url,
+    cat.sort AS cat_sort,
     concat(replace(cat.name, '/', '|'), '/', tax.name) AS sort
   FROM
     ae.taxonomy tax
-    INNER JOIN ae.tree_category cat ON tax.tree_category = cat.id
+    INNER JOIN ae.tree_category cat ON tax.tree_category = cat.id,
+    constants c
+  WHERE
+    c.active_url LIKE replace(cat.name, '/', '|') || '%'
 ),
 objects AS (
   WITH RECURSIVE a AS (
@@ -63,14 +59,17 @@ objects AS (
       ae.object.parent_id,
       3::bigint AS level,
       cat.name AS category,
+      cat.sort AS cat_sort,
       concat(replace(cat.name, '/', '|'), '/', ae.taxonomy.id, '/', ae.object.id) AS url,
       concat(replace(cat.name, '/', '|'), '/', replace(ae.taxonomy.name, '/', '|'), '/', replace(ae.object.name, '/', '|')) AS sort
     FROM
       ae.object
       INNER JOIN ae.taxonomy
-      INNER JOIN ae.tree_category cat ON ae.taxonomy.tree_category = cat.id ON ae.object.taxonomy_id = ae.taxonomy.id
+      INNER JOIN ae.tree_category cat ON ae.taxonomy.tree_category = cat.id ON ae.object.taxonomy_id = ae.taxonomy.id,
+      constants c
     WHERE
       ae.object.parent_id IS NULL
+      AND c.active_url LIKE concat(replace(cat.name, '/', '|'), '/', ae.taxonomy.id) || '%'
     UNION ALL
     SELECT
       o.id,
@@ -78,19 +77,23 @@ objects AS (
       o.parent_id,
       a.level + 1,
       cat.name AS category,
+      cat.sort AS cat_sort,
       concat(a.url, '/', o.id) AS url,
       concat(a.sort, '/', replace(o.name, '/', '|')) AS sort
     FROM
       ae.object o
       INNER JOIN ae.taxonomy
       INNER JOIN ae.tree_category cat ON ae.taxonomy.tree_category = cat.id ON o.taxonomy_id = ae.taxonomy.id
-      JOIN a ON a.id = o.parent_id
+      JOIN a ON a.id = o.parent_id,
+      constants c
     WHERE
       a.level <= 10
+      AND c.active_url LIKE a.url || '%'
 )
     SELECT
       level,
       category,
+      cat_sort,
       name,
       id,
       parent_id,
@@ -102,6 +105,7 @@ objects AS (
   SELECT
     level,
     category,
+    cat_sort,
     name,
     id,
     parent_id,
@@ -113,6 +117,7 @@ objects AS (
   SELECT
     level,
     category,
+    cat_sort,
     name,
     id,
     parent_id,
@@ -124,6 +129,7 @@ objects AS (
   SELECT
     level,
     category,
+    cat_sort,
     name,
     id,
     parent_id,
@@ -131,7 +137,7 @@ objects AS (
     sort
   FROM
     tree_categories
-    -- maybe not sort to make query faster?
   ORDER BY
+    cat_sort,
     sort;
 
