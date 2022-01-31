a26f07d393058beae91841ffaf678a4397086ffc
Alexander Gabriel <alex.barbalex@gmail.com>
2022-01-31 13:49:48 +0100

    work on tree query

--- src/sql/tree_by_active_url.sql
+++ src/sql/tree_by_active_url.sql
@@ -1,8 +1,10 @@
 /**
  * this query is currently not in use
  * would be a very nice way to fetch data for the tree:
- * filter by level and category
+ * filter by passing activeNodeArray
+ *
  * data for users and organizations could be added
+ *
  * ISSUE: if used as view policies would not be respected :-(
  * see: https://www.postgresql.org/docs/13/sql-createpolicy.html
  * "This does not change how views work, however. As with normal queries and views, 
@@ -14,8 +16,6 @@
  * Seems that yes: functions have SECURITY INVOKER set by default
  * see postgresql.org/docs/current/sql-createfunction.html
  *
- * would need to pass in activeNodeArray to create full tree?
- *
  * TODO: how return exactly what is shown? (openNodes)
  * needs to be queried with help of activeUrl:
  * - break activeUrl up into parent levels
@@ -23,13 +23,12 @@
  *   - this object's url equals activeUrl 
  *   - or: this object's url minus last element equals any of activeUrl's parents 
  *
- * ONLY works on PG >= 14
  */
 -- https://stackoverflow.com/a/16552441/712005
 WITH constants (
   active_url
 ) AS (
-  VALUES ('Arten/aed47d40-7b0e-11e8-b9a5-bd4f79edbcc4/aedf52b0-7b0e-11e8-b9a5-bd4f79edbcc4/aee40da0-7b0e-11e8-b9a5-bd4f79edbcc4/aef3c510-7b0e-11e8-b9a5-bd4f79edbcc4/798695e7-65ea-4e63-90e4-c8a393233f2e')
+  VALUES ('https://www.arteigenschaften.ch/Eigenschaften-Sammlungen/bdf5c7c0-7b0e-11e8-b9a5-bd4f79edbcc4')
 ),
 tree_categories AS (
   SELECT
@@ -152,7 +151,8 @@ objects AS (
       parent_id,
       url,
       sort,
-      children_count
+      children_count,
+      children_count::text AS info
     FROM
       objects
     UNION ALL
@@ -165,7 +165,8 @@ objects AS (
       parent_id,
       url,
       sort,
-      children_count
+      children_count,
+      children_count::text AS info
     FROM
       taxonomies
     UNION ALL
@@ -178,7 +179,12 @@ objects AS (
       parent_id,
       url,
       sort,
-      children_count
+      children_count,
+      CASE WHEN name = 'Eigenschaften-Sammlungen' THEN
+        children_count::text
+      ELSE
+        children_count::text || ' Taxonomien'
+      END AS info
     FROM
       tree_categories
     ORDER BY
