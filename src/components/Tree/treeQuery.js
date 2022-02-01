fc4da86efdf8afcf5b6a0369b7e59203d4ea0c9f
Alexander Gabriel <alex.barbalex@gmail.com>
2022-02-01 20:41:44 +0100

    optimize

--- src/components/Tree/treeQuery.js
+++ src/components/Tree/treeQuery.js
@@ -1,44 +1,13 @@
 import { gql } from '@apollo/client'
 
 export default gql`
-  query TreeDataQuery(
-    $existsLevel2Benutzer: Boolean!
-    $username: String!
-    $url: [String]
-    $hasToken: Boolean!
-  ) {
+  query TreeDataQuery($username: String!, $url: [String], $hasToken: Boolean!) {
     userByName(name: $username) {
       id
-      name
-      email
       organizationUsersByUserId {
         nodes {
           id
-          organizationId
           role
-          organizationByOrganizationId {
-            id
-            name
-          }
-        }
-      }
-    }
-    allUsers(orderBy: NAME_ASC) {
-      totalCount
-      nodes {
-        id
-        name @include(if: $existsLevel2Benutzer)
-        email @include(if: $existsLevel2Benutzer)
-        organizationUsersByUserId @include(if: $existsLevel2Benutzer) {
-          nodes {
-            id
-            organizationId
-            role
-            organizationByOrganizationId {
-              id
-              name
-            }
-          }
         }
       }
     }
--- src/components/Tree/treeQueryVariables.js
+++ src/components/Tree/treeQueryVariables.js
@@ -4,8 +4,6 @@ const treeQueryVariables = (store) => {
   const activeNodeArray = getSnapshot(store.activeNodeArray)
 
   return {
-    existsLevel2Benutzer:
-      activeNodeArray[0] === 'Benutzer' && !!store.login.token,
     username: store.login.username ?? 'no_user_with_this_name_exists',
     url: activeNodeArray.filter((n) => n !== 0),
     hasToken: !!store.login.token,
