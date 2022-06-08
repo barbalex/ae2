e3e0eb872ec1dacdfb6898aae2e56fb6b613b96c
Alexander Gabriel <alex.barbalex@gmail.com>
2022-06-08 11:37:24 +0200

    update deps

--- src/components/Layout/AppBar/MoreMenu.js
+++ src/components/Layout/AppBar/MoreMenu.js
@@ -122,7 +122,7 @@ const MoreMenu = () => {
         <MenuItem onClick={onClickUptime}>
           {`Verfügbarkeit der Server von ${hostname}`}
         </MenuItem>
-        <Version>Version: 1.7.12 vom 3.06.2022</Version>
+        <Version>Version: 1.7.13 vom 8.06.2022</Version>
       </Menu>
     </div>
   )
