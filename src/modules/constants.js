3a5a1a08c19f75cd8b21eb555a54427f2daec486
Alexander Gabriel <alex.barbalex@gmail.com>
2022-12-17 22:25:33 +0100

    up max fields

--- src/modules/constants.js
+++ src/modules/constants.js
@@ -1,7 +1,7 @@
 const constants = () => ({
   appBarHeight: 64,
   columnWidth: 600,
-  export: { properties: { columnWidth: 400 }, maxFields: 35 },
+  export: { properties: { columnWidth: 400 }, maxFields: 100 },
   altTaxonomies: ['CSCF (2009)', 'NISM (2010)', 'SISF (2005)'],
   sidebar: {
     width: 420,
