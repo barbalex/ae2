bcb9555dc0f88bebd2aeb77c9d450c05b3a7de97
Alexander Gabriel <alex.barbalex@gmail.com>
2022-12-18 21:17:02 +0100

    style

--- src/components/shared/DataTable.jsx
+++ src/components/shared/DataTable.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import Table from '@mui/material/Table'
 import TableBody from '@mui/material/TableBody'
 import TableCell from '@mui/material/TableCell'
-import TableContainer from '@mui/material/TableContainer'
 import TableHead from '@mui/material/TableHead'
 import TableRow from '@mui/material/TableRow'
 import TableSortLabel from '@mui/material/TableSortLabel'
-import Paper from '@mui/material/Paper'
 import Box from '@mui/material/Box'
 import { visuallyHidden } from '@mui/utils'
+import styled from '@emotion/styled'
+
+const StyledTableRow = styled(TableRow)`
+  padding: 0 5px;
+`
 
 // data is array of objects
 // keys are column names
@@ -17,78 +20,94 @@ const DataTable = ({ data, order = 'ASC', orderBy = 'id', setOrder }) => {
   const columnNames = Object.keys(data[0])
 
   return (
-    <TableContainer component={Paper}>
-      <Table
-        sx={{ minWidth: 650, fontSize: 'small !important' }}
-        size="small"
-        aria-label="Export Vorschau"
-        stickyHeader={true}
-      >
-        <TableHead sx={{ backgroundColor: '#f6f6f6' }}>
-          <TableRow>
-            {columnNames.map((name) => {
-              if (name === 'id') {
-                return (
-                  <TableCell key={name} sx={{ minWidth: 270 }}>
-                    {name}
-                  </TableCell>
-                )
-              }
-
+    <Table
+      sx={{
+        minWidth: 650,
+        fontSize: 'small !important',
+        backgroundColor: 'white',
+        marginBottom: 0,
+      }}
+      size="small"
+      aria-label="Export Vorschau"
+      stickyHeader={true}
+    >
+      <TableHead>
+        <StyledTableRow>
+          {columnNames.map((name) => {
+            if (name === 'id') {
               return (
                 <TableCell
                   key={name}
-                  sortDirection={orderBy === name ? order.toLowerCase() : false}
+                  sx={{
+                    minWidth: 270,
+                    '&:first-child': { paddingLeft: '6px !important' },
+                    backgroundColor: '#ffcc80 !important',
+                  }}
                 >
-                  <TableSortLabel
-                    active={orderBy === name}
-                    direction={orderBy === name ? order.toLowerCase() : 'asc'}
-                    onClick={() =>
-                      setOrder({
-                        by: name,
-                        direction: order === 'ASC' ? 'DESC' : 'ASC',
-                      })
-                    }
-                  >
-                    {name}
-                    {orderBy === name ? (
-                      <Box component="span" sx={visuallyHidden}>
-                        {order === 'DESC'
-                          ? 'sorted descending'
-                          : 'sorted ascending'}
-                      </Box>
-                    ) : null}
-                  </TableSortLabel>
+                  {name}
                 </TableCell>
               )
-            })}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {data.map((row) => {
-            const keys = Object.keys(row).filter((k) => k !== 'id')
+            }
 
             return (
-              <TableRow
-                key={row.id}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              <TableCell
+                key={name}
+                sortDirection={orderBy === name ? order.toLowerCase() : false}
+                sx={{
+                  '&:first-child': { paddingLeft: '6px !important' },
+                  backgroundColor: '#ffcc80 !important',
+                }}
               >
-                <TableCell
-                  component="th"
-                  scope="row"
-                  sx={{ fontSize: '0.8rem' }}
+                <TableSortLabel
+                  active={orderBy === name}
+                  direction={orderBy === name ? order.toLowerCase() : 'asc'}
+                  onClick={() =>
+                    setOrder({
+                      by: name,
+                      direction: order === 'ASC' ? 'DESC' : 'ASC',
+                    })
+                  }
                 >
-                  {row.id}
-                </TableCell>
-                {keys.map((key) => (
-                  <TableCell key={key}>{row[key]}</TableCell>
-                ))}
-              </TableRow>
+                  {name}
+                  {orderBy === name ? (
+                    <Box component="span" sx={visuallyHidden}>
+                      {order === 'DESC'
+                        ? 'sorted descending'
+                        : 'sorted ascending'}
+                    </Box>
+                  ) : null}
+                </TableSortLabel>
+              </TableCell>
             )
           })}
-        </TableBody>
-      </Table>
-    </TableContainer>
+        </StyledTableRow>
+      </TableHead>
+      <TableBody>
+        {data.map((row) => {
+          const keys = Object.keys(row).filter((k) => k !== 'id')
+
+          return (
+            <StyledTableRow
+              key={row.id}
+              sx={{
+                '&:last-child td, &:last-child th': { border: 0 },
+              }}
+            >
+              <TableCell
+                component="th"
+                scope="row"
+                sx={{ fontSize: '0.8rem', paddingLeft: '6px !important' }}
+              >
+                {row.id}
+              </TableCell>
+              {keys.map((key) => (
+                <TableCell key={key}>{row[key]}</TableCell>
+              ))}
+            </StyledTableRow>
+          )
+        })}
+      </TableBody>
+    </Table>
   )
 }
 
