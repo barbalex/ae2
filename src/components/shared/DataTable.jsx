194cd28533ad94c2fd4327fb6490d98eeb9bfa23
Alexander Gabriel <alex.barbalex@gmail.com>
2022-12-21 15:52:47 +0100

    optimize

--- src/components/shared/DataTable.jsx
+++ src/components/shared/DataTable.jsx
@@ -8,7 +8,12 @@ import TableSortLabel from '@mui/material/TableSortLabel'
 import Box from '@mui/material/Box'
 import { visuallyHidden } from '@mui/utils'
 import styled from '@emotion/styled'
+import SimpleBar from 'simplebar-react'
 
+const Container = styled.div`
+  width: 100%;
+  overflow-x: auto;
+`
 const StyledTableRow = styled(TableRow)`
   padding: 0 5px;
 `
@@ -25,98 +30,103 @@ const DataTable = ({
 }) => {
   const columnNames = Object.keys(data[0])
 
-  console.log('DataTable, columnNames:', columnNames)
-
   return (
-    <Table
-      sx={{
-        minWidth: 650,
-        fontSize: 'small !important',
-        backgroundColor: 'white',
-        marginBottom: 0,
-      }}
-      size="small"
-      aria-label="Export Vorschau"
-      stickyHeader={true}
-    >
-      <TableHead>
-        <StyledTableRow>
-          {columnNames.map((name) => {
-            if (name === idKey) {
+    <Container>
+      <SimpleBar style={{ maxHeight: '100%', height: '100%' }}>
+        <Table
+          sx={{
+            minWidth: 650,
+            fontSize: 'small !important',
+            backgroundColor: 'white',
+            marginBottom: 0,
+          }}
+          size="small"
+          aria-label="Export Vorschau"
+          stickyHeader={true}
+        >
+          <TableHead>
+            <StyledTableRow>
+              {columnNames.map((name) => {
+                if (name === idKey) {
+                  return (
+                    <TableCell
+                      key={name}
+                      sx={{
+                        minWidth: 270,
+                        '&:first-of-type': { paddingLeft: '6px !important' },
+                        backgroundColor: '#ffcc80 !important',
+                      }}
+                    >
+                      {name}
+                    </TableCell>
+                  )
+                }
+
+                return (
+                  <TableCell
+                    key={name}
+                    sortDirection={
+                      orderBy === name ? order.toLowerCase() : false
+                    }
+                    sx={{
+                      '&:first-of-type': { paddingLeft: '6px !important' },
+                      backgroundColor: '#ffcc80 !important',
+                      // prevent hideous high headers
+                      minWidth: 200,
+                    }}
+                  >
+                    <TableSortLabel
+                      active={orderBy === name}
+                      direction={orderBy === name ? order.toLowerCase() : 'asc'}
+                      onClick={() =>
+                        setOrder({
+                          by: name,
+                          direction: order === 'ASC' ? 'DESC' : 'ASC',
+                        })
+                      }
+                    >
+                      {name}
+                      {orderBy === name ? (
+                        <Box component="span" sx={visuallyHidden}>
+                          {order === 'DESC'
+                            ? 'sorted descending'
+                            : 'sorted ascending'}
+                        </Box>
+                      ) : null}
+                    </TableSortLabel>
+                  </TableCell>
+                )
+              })}
+            </StyledTableRow>
+          </TableHead>
+          <TableBody>
+            {data.map((row) => {
+              const keys = Object.keys(row).filter((k) => k !== idKey)
+
               return (
-                <TableCell
-                  key={name}
+                <StyledTableRow
+                  key={row[idKey]}
                   sx={{
-                    minWidth: 270,
-                    '&:first-of-type': { paddingLeft: '6px !important' },
-                    backgroundColor: '#ffcc80 !important',
+                    '&:last-child td, &:last-child th': { border: 0 },
                   }}
                 >
-                  {name}
-                </TableCell>
+                  <TableCell
+                    component="th"
+                    scope="row"
+                    sx={{ fontSize: '0.8rem', paddingLeft: '6px !important' }}
+                  >
+                    {row[idKey]}
+                  </TableCell>
+                  {keys.map((key) => (
+                    <TableCell key={key}>{row[key]}</TableCell>
+                  ))}
+                </StyledTableRow>
               )
-            }
-
-            return (
-              <TableCell
-                key={name}
-                sortDirection={orderBy === name ? order.toLowerCase() : false}
-                sx={{
-                  '&:first-of-type': { paddingLeft: '6px !important' },
-                  backgroundColor: '#ffcc80 !important',
-                }}
-              >
-                <TableSortLabel
-                  active={orderBy === name}
-                  direction={orderBy === name ? order.toLowerCase() : 'asc'}
-                  onClick={() =>
-                    setOrder({
-                      by: name,
-                      direction: order === 'ASC' ? 'DESC' : 'ASC',
-                    })
-                  }
-                >
-                  {name}
-                  {orderBy === name ? (
-                    <Box component="span" sx={visuallyHidden}>
-                      {order === 'DESC'
-                        ? 'sorted descending'
-                        : 'sorted ascending'}
-                    </Box>
-                  ) : null}
-                </TableSortLabel>
-              </TableCell>
-            )
-          })}
-        </StyledTableRow>
-      </TableHead>
-      <TableBody>
-        {data.map((row) => {
-          const keys = Object.keys(row).filter((k) => k !== idKey)
-          console.log({ row })
-
-          return (
-            <StyledTableRow
-              key={row[idKey]}
-              sx={{
-                '&:last-child td, &:last-child th': { border: 0 },
-              }}
-            >
-              <TableCell
-                component="th"
-                scope="row"
-                sx={{ fontSize: '0.8rem', paddingLeft: '6px !important' }}
-              >
-                {row[idKey]}
-              </TableCell>
-              {keys.map((key) => (
-                <TableCell key={key}>{row[key]}</TableCell>
-              ))}
-            </StyledTableRow>
-          )
-        })}
-      </TableBody>
-    </Table>
+            })}
+          </TableBody>
+        </Table>
+      </SimpleBar>
+    </Container>
   )
 }
 
