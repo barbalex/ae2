c040a91cdda05c0109e9d685cb55fdb41fb5c713
Alexander Gabriel <alex.barbalex@gmail.com>
2022-12-23 15:02:09 +0100

    work on export rco properties

--- src/sql/export_rco_properties.sql
+++ src/sql/export_rco_properties.sql
@@ -55,50 +55,50 @@ STABLE;
 
 ALTER FUNCTION ae.export_rco_count (export_taxonomies text[]) OWNER TO postgres;
 
-WITH property_list AS (
-  SELECT DISTINCT
-    pc.id AS pc_id,
-    rco.relation_type,
-    jsonb_object_keys(rco.properties) AS property
-  FROM
-    ae.relation rco
-    INNER JOIN ae.object object ON object.id = rco.object_id
-    INNER JOIN ae.taxonomy tax ON tax.id = object.taxonomy_id
-    INNER JOIN ae.property_collection pc ON pc.id = rco.property_collection_id
-  WHERE
-    tax.name IN ('SISF (2005)')
-),
-property_count AS (
-  SELECT
-    pc_id,
-    relation_type,
-    count(property) AS count
-  FROM
-    property_list
-  GROUP BY
-    pc_id,
-    relation_type
-),
-relation_list AS (
-  SELECT DISTINCT
-    pc.id AS pc_id,
-    pc.name,
-    rco.relation_type
-  FROM
-    ae.relation rco
-    INNER JOIN ae.object object ON object.id = rco.object_id
-    INNER JOIN ae.taxonomy tax ON tax.id = object.taxonomy_id
-    INNER JOIN ae.property_collection pc ON pc.id = rco.property_collection_id
-  WHERE
-    tax.name IN ('SISF (2005)'))
-SELECT
-  count(relation_list.name) AS pc_reltype_count,
-  sum(property_count.count) AS property_count
-FROM
-  relation_list
-  LEFT JOIN property_count ON property_count.pc_id = property_count.pc_id
-    AND relation_list.relation_type = property_count.relation_type;
-
+-- example query:
+-- WITH property_list AS (
+--   SELECT DISTINCT
+--     pc.id AS pc_id,
+--     rco.relation_type,
+--     jsonb_object_keys(rco.properties) AS property
+--   FROM
+--     ae.relation rco
+--     INNER JOIN ae.object object ON object.id = rco.object_id
+--     INNER JOIN ae.taxonomy tax ON tax.id = object.taxonomy_id
+--     INNER JOIN ae.property_collection pc ON pc.id = rco.property_collection_id
+--   WHERE
+--     tax.name IN ('SISF (2005)')
+-- ),
+-- property_count AS (
+--   SELECT
+--     pc_id,
+--     relation_type,
+--     count(property) AS count
+--   FROM
+--     property_list
+--   GROUP BY
+--     pc_id,
+--     relation_type
+-- ),
+-- relation_list AS (
+--   SELECT DISTINCT
+--     pc.id AS pc_id,
+--     pc.name,
+--     rco.relation_type
+--   FROM
+--     ae.relation rco
+--     INNER JOIN ae.object object ON object.id = rco.object_id
+--     INNER JOIN ae.taxonomy tax ON tax.id = object.taxonomy_id
+--     INNER JOIN ae.property_collection pc ON pc.id = rco.property_collection_id
+--   WHERE
+--     tax.name IN ('SISF (2005)'))
+-- SELECT
+--   count(relation_list.name) AS pc_reltype_count,
+--   sum(property_count.count) AS property_count
+-- FROM
+--   relation_list
+--   LEFT JOIN property_count ON property_count.pc_id = property_count.pc_id
+--     AND relation_list.relation_type = property_count.relation_type;
 CREATE TYPE rco_property_count AS (
   pcname text,
   relationtype text,
