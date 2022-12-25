2c8136b3331bbff9de7b51e2003baa9a17b61dc7
Alexander Gabriel <alex.barbalex@gmail.com>
2022-12-25 13:37:21 +0100

    optimize

--- src/components/Tree/index.js
+++ src/components/Tree/index.js
@@ -177,17 +177,17 @@ const TreeComponent = () => {
 
   const client = useApolloClient()
 
-  console.log('TreeComponent', {
-    activeNodeArray,
-    username: login.username,
-    tokenExists: !!login.token,
-    variables,
-  })
+  // console.log('TreeComponent', {
+  //   activeNodeArray,
+  //   username: login.username,
+  //   tokenExists: !!login.token,
+  //   variables,
+  // })
 
   const { isLoading, error, data } = useQuery({
     queryKey: ['treeQuery', Object.values(variables)],
-    queryFn: async () =>
-      await client.query({
+    queryFn: () =>
+      client.query({
         query: treeQuery,
         variables,
         // seems that react-query cache is not working
