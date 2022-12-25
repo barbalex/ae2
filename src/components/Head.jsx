895eedd104be6431d388d5aaeea64f89c4b027c1
Alexander Gabriel <alex.barbalex@gmail.com>
2022-12-25 12:45:16 +0100

    up version

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Head = () => (
   <>
-    <title>arteigenschaften v1.8.11</title>
+    <title>arteigenschaften v1.8.12</title>
     <meta name="description" content="Arteigenschaften sammeln" />
     <meta
       name="keywords"
