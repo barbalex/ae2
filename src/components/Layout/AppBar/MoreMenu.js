edf2be02334ddaae0b1b4da55468a90cf60277dd
Alexander Gabriel <alex.barbalex@gmail.com>
2022-12-25 13:02:38 +0100

    up version

--- src/components/Layout/AppBar/MoreMenu.js
+++ src/components/Layout/AppBar/MoreMenu.js
@@ -122,7 +122,7 @@ const MoreMenu = () => {
         <MenuItem onClick={onClickUptime}>
           {`Verfügbarkeit der Server von ${hostname}`}
         </MenuItem>
-        <Version>Version: 1.8.12 vom 25.12.2022</Version>
+        <Version>Version: 1.8.13 vom 25.12.2022</Version>
       </Menu>
     </div>
   )
